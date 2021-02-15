8f215d79e63b9e9839d236dd24191918186667ed
Adel Mindubaev <cobara@student.21-school.ru>
2021-02-15 21:33:12 +0300

    create contoller directory

--- src/main/java/in/russia/demo/controller/HomeController.java
+++ src/main/java/in/russia/demo/controller/HomeController.java
@@ -1,13 +1,15 @@
-package in.russia.demo;
+package in.russia.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class controller {
+public class HomeController {
 
     @GetMapping("/")
     public String getHomePage(){
         return "index";
     }
+
+    
 }
